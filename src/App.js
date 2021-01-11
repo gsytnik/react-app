875d1ec5daa19b754e902e9cc758571c67ad98ca
Gennady Sytnik <gsytnik@calpoly.edu>
2021-01-11 15:59:42 -0800

    form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Table from './Table'
+import Form from './Form'
 
 class App extends Component {
   
@@ -40,6 +41,7 @@ class App extends Component {
 		return (
 			<div className="container">
 				<Table characterData={characters} removeCharacter={this.removeCharacter} />
+				<Form />
 			</div>
 		);
 	}
